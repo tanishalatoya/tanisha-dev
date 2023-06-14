69f94c4f0c3f95f5fc8b704565670231c5b4c4ab
Tanisha L Davey <tanisha.davey2@gmail.com>
2023-06-14 09:48:38 -0600

    Udpated index.js to render <App />

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/App/App";
 
-const container = document.getElementById("ccontainer");
+const container = document.getElementById("container");
 
 const root = createRoot(container);
 
-root.render(App);
+root.render(<App />);
