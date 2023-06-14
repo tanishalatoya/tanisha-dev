4849153f62b858da7f3665a678f0db0ee8c40032
Tanisha L Davey <tanisha.davey2@gmail.com>
2023-06-14 17:29:19 -0600

    Removed typos and syntax errors

--- index.jsx
+++ index.jsx
@@ -8,5 +8,5 @@ const root = createRoot(container);
 root.render(<App />);
 
 console.log(
-  "Hiya 👋, \n\nThanks for stopping by my page! I hope you're able to glean some insight into my passion for beign a software developer from what you've seen here. I am currently interested in a landing a new role with a preference for fullstack or frontend roles. Feel free to reach out to me if you think I'd be a good fit for your company or just to chat about software! \n\nI can be reached using any of the links of my page or via: tanisha.davey2@gmail.com. \n\nFeel free to mention that you saw this console.log!"
+  "Hiya 👋, \n\nThanks for stopping by my page! I hope you're able to glean some insight into my passion for being a software developer from what you've seen here. I am currently interested in landing a new role with a preference for fullstack or frontend roles. Feel free to reach out to me if you think I'd be a good fit for your company or just to chat about software! \n\nI can be reached using any of the links on my page or via: tanisha.davey2@gmail.com. \n\nFeel free to mention that you saw this console.log!"
 );
